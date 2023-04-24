cbb34c048b71e402ac5b2554560ff046e0f9aff1
Jesse Ilc <jesseilc123@gmail.com>
2023-04-24 16:47:24 -0500

    lab complete

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+        {actors.map((actor) => (
+          <div key={actor.name}>
+            {actor.name}
+            <div>
+              {actor.movies.map((movie) => (
+                <li key={movie}>{movie}</li>
+              ))}
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      <div>
+        {directors.map((director) => (
+          <div key={director.name}>
+            {director.name}
+            <div>
+              {director.movies.map((movie) => (
+                <li key={movie}>
+                  {movie}
+                </li>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Directors;
